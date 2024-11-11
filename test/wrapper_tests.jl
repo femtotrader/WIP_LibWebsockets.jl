11c4da1ce7b9ed694cb63542c66615945c920559
FemtoTrader <femto.trader@gmail.com>
2024-11-11 09:31:44 +0000

    WIP

--- test/wrapper_tests.jl
+++ test/wrapper_tests.jl
@@ -1,1 +1,8 @@
-@testset "wrapper_tests" begin end
+using LibWebsockets.LibWebsocketsWrapper: lws_get_library_version
+
+
+@testset "wrapper_tests" begin
+    @testset "lws_get_library_version" begin
+        @test unsafe_string(lws_get_library_version()) == "4.3.3-v4.3.3"
+    end
+end
